5c955163043180cafebddf9a61f826735740cf98
Thomas E <spmte@runbox.com>
2014-07-13 18:36:02 +0200

    Solution

--- plot3.R
+++ plot3.R
@@ -32,7 +32,7 @@ png("plot3.png", width=480, height=480)
 plot(data$Sub_metering_1, ylab="Energy sub metering", xlab="", type="l", xaxt="n")
 points(data$Sub_metering_2, col="red", type="l")
 points(data$Sub_metering_3, col="blue", type="l")
-legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), text.col=c("black", "red", "blue"), col=c("black", "red", "blue"), lty=1)
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1)
 axis(1, at=x_vals, lab=x_labels)
 
 dev.off()
